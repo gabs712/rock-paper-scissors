f9ab41204845b0a2ba58509ef47b7f8acea1dd5e
gabs <gabs712.g@gmail.com>
2024-01-29 15:06:36 -0300

    Add PlayRound function

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+// Get a random number for the computer
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
 
-  if (randomNumber === 0) return 'Rock';
-  else if (randomNumber === 1) return 'Paper';
-  else if (randomNumber === 2) return 'Scissors'
+  if (randomNumber === 0) return 'rock';
+  else if (randomNumber === 1) return 'paper';
+  else if (randomNumber === 2) return 'scissors';
 }
-console.log(getComputerChoice())
+
+// Choose the winner based on rock paper scissors rules
+function playRound(playerSelecction, computerSelection) {
+  if (playerSelecction === computerSelection) {
+    const capitalizedPlayerSelecction = playerSelecction[0].toUpperCase() + playerSelecction.slice(1);
+    const capitalizedComputerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1);
+
+    return `Tie! ${capitalizedPlayerSelecction} ties with ${capitalizedComputerSelection}`;
+  }
+
+  else if (playerSelecction === 'rock') {
+    if (computerSelection === 'paper') return `You Lose! Paper beats Rock`;
+    else if (computerSelection === 'scissors') return `You Win! Rock beats Scissors`
+  }
+
+  else if (playerSelecction === 'paper') {
+    if (computerSelection === 'rock') return `You win! Paper beats Rock`;
+    else if (computerSelection === 'scissors') return `You lose! Scissors beats Paper`;
+  }
+
+  else if (playerSelecction === 'scissors') {
+    if (computerSelection === 'rock') return `You Lose! Rock beats Scissors`;
+    else if (computerSelection === 'paper') return `You Win! Scissors beats Paper!`;
+  }
+}
