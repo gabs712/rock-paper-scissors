4325765968c2ce2de003f4d11d18d3e19cf115cd
gabs <gabs712.g@gmail.com>
2024-01-29 14:18:57 -0300

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('ok')
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  if (randomNumber === 0) return 'Rock';
+  else if (randomNumber === 1) return 'Paper';
+  else if (randomNumber === 2) return 'Scissors'
+}
+console.log(getComputerChoice())
